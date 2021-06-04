c60a170a862b5fd7cc89e19dd2fef5ec85a7b366
Mab <malikbello084@gmail.com>
2021-06-04 11:50:03 +0100

    Added Instructions to UI

--- app/src/main/java/com/codewithmab/myapplication/CGPA_Calculator.java
+++ app/src/main/java/com/codewithmab/myapplication/CGPA_Calculator.java
@@ -1,14 +1,42 @@
 package com.codewithmab.myapplication;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.text.HtmlCompat;
 
 public class CGPA_Calculator extends AppCompatActivity {
+    LinearLayout cgpaCalcLinearLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cgpa_calculator);
+
+        cgpaCalcLinearLayout = findViewById(R.id.cgpaCalcLinearLayout);
+        popupInstructions();
+
+    }
+
+    void popupInstructions(){
+        CardView instructionCardView = (CardView) LayoutInflater.from(this).inflate(R.layout.instructions, null);
+        TextView instructionTextView = instructionCardView.findViewById(R.id.instructionTextView);
+        instructionTextView.setText(getString(R.string.app_info));
+        cgpaCalcLinearLayout.addView(instructionCardView);
+
+        instructionCardView = (CardView) LayoutInflater.from(this).inflate(R.layout.instructions, null);
+        instructionTextView = instructionCardView.findViewById(R.id.instructionTextView);
+        instructionTextView.setText(HtmlCompat.fromHtml(getString(R.string.instruction_details), HtmlCompat.FROM_HTML_MODE_LEGACY));
+        cgpaCalcLinearLayout.addView(instructionCardView);
+
+        instructionCardView = (CardView) LayoutInflater.from(this).inflate(R.layout.instructions, null);
+        instructionTextView = instructionCardView.findViewById(R.id.instructionTextView);
+        instructionTextView.setText(HtmlCompat.fromHtml(getString(R.string.instruction_addcourse), HtmlCompat.FROM_HTML_MODE_LEGACY));
+        cgpaCalcLinearLayout.addView(instructionCardView);
     }
 }
+
